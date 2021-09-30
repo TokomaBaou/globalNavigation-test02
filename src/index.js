c61cb2913a108b56ab8a4340d380c83bb26a5a71
MAKOTO OBA <86038701+TokomaBaou@users.noreply.github.com>
2021-09-30 17:49:11 +0900

    [add]css cssスタイルの追加

--- src/index.js
+++ src/index.js
@@ -2,4 +2,18 @@ import "./styles.css";
 
 document.getElementById("app").innerHTML = `
 <h1>グローバルナビゲーション</h1>
+
+<div class="openbtn"><span></span><span></span><span></span></div>
+<nav id="g-nav">
+  <div id="g-nav-list">
+    <!--縦スクロール用のdiv-->
+    <ul>
+      <li><a href="#">Top</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Service</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+  </div>
+</nav>
+
 `;
